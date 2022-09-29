c8e6c3fb356af305e155abab4e715af63fd6c0eb
xuruijiang <2451603830@qq.com>
2022-09-29 10:52:32 +0800

    每日功法

--- test_9_29_theLongestPublicPrefix/main.c
+++ test_9_29_theLongestPublicPrefix/main.c
@@ -5,43 +5,30 @@
 char * longestCommonPrefix(char ** strs, int strsSize){
 
     if (strsSize < 2 ){
-
         return strs[0];
-    }
-    {
-        /* code */
     }
 
     int j = 0;
-
-    printf("%c\n",*(strs[0] + 3));
-
     int i = 0;
     int flag = 0;
+
+    printf("%c\n",*(strs[0] + 3));//这里打印都没问题
     for ( i = 0; ; ++i) {
         for ( j = 1; j < strsSize; ++j) {
             if(*(strs[j] + i) == '\0' || *(strs[j] + i) != *(strs[j - 1] + i)){
                 flag = 1;
-                *(strs[0] + i) = '\0';
+                *(strs[0] + i) = '\0'; //说我这里有问题
                 break;
             }
-
         }
-
         if(flag == 1){
             break;
         }
     }
 
-    // if(flag == 1){
-    //     *(strs[0] + i) = '\0';
-    // }
-
     return strs[0];
-
 }
 int main() {
-
 
     char *str[5] = {"abcteyd","abcccvv","abceggg","abclfkf","abcldud"};
 
