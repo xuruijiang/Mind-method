89685d3e53495105c756f5a597717f293e0a931a
xuruijiang <2451603830@qq.com>
2022-11-02 00:06:10 +0800

    每日功法

---
+++ test_10_29/test_10_29/main.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  test_10_29
+//
+//  Created by 徐瑞江 on 2022/10/29.
+//
+
+#include <stdio.h>
+
+
+int fun(int a){
+    return 0;
+}
+
+int main() {
+
+
+    int a = 5;
+    int b = 6;
+    printf("%d",fun(a));
+    return 0;
+}
