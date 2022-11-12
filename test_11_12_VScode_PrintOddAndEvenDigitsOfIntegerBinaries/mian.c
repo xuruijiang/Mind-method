8d7e3a07017e5bcfdf988123b6f2a9705ca16473
xuruijiang <2451603830@qq.com>
2022-11-13 00:12:40 +0800

    每日功法

---
+++ test_11_12_VScode_PrintOddAndEvenDigitsOfIntegerBinaries/mian.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main(){
+
+    int num = 0;
+    scanf("%d",&num);
+    int oddNumber[16] = {0};
+    int evenNumber[16] = {0};
+    int numCopy = num;
+
+    for (int i = 0; i < 15; i++)
+    {
+        if((numCopy & 1) == 1 ){
+            oddNumber[i] = 1;
+        }else{
+            oddNumber[i] = 0;
+        } 
+
+        numCopy = numCopy >> 2;
+    }
+    
+    numCopy = num >> 1;
+    for (int i = 0; i < 15; i++)
+    {
+        
+        if((numCopy & 1) == 1 ){
+            evenNumber[i] = 1;
+        }else{
+            evenNumber[i] = 0;
+        } 
+
+        numCopy = numCopy >> 2;
+    }
+
+    for (int i = 15; i >= 0; i--)
+    {
+        /* code */
+        printf("%d",oddNumber[i]);
+    }
+    printf("\n");
+    for (int i = 15; i >= 0; i--)
+    {
+        /* code */
+        printf("%d",evenNumber[i]);
+    }
+
+    return 0;
+}
