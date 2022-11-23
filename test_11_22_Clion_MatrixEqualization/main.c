45a75bf456c73c92fa963bef9b28148826477d7e
xuruijiang <2451603830@qq.com>
2022-11-23 09:06:44 +0800

    每日功法

---
+++ test_11_22_Clion_MatrixEqualization/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n = 0;
+    int m = 0;
+    int count = 0;
+    scanf("%d%d",&n,&m);
+
+    int *pIntOne = (int *)malloc(sizeof(int) * n * m);
+    int *pIntTwo = (int *)malloc(sizeof(int) * n * m);
+
+    for (int i = 0; i < n * m; ++i) {
+
+        scanf("%d",(pIntOne + i));
+    }
+    for (int j = 0; j < n * m; ++j) {
+
+        scanf("%d",(pIntTwo + j));
+    }
+
+    for (int i = 0; i < n * m; ++i) {
+
+        if(pIntOne[i] != pIntTwo[i]){
+
+            count  = 1;
+            break;
+        }
+    }
+
+    if(1 == count){
+
+        printf("No\n");
+    } else{
+        printf("Yes\n");
+    }
+    return 0;
+}
