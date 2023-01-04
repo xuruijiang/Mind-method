928565365bff73f5ffcb4bd1c4aa2ef3cc7cb042
xuruijiang <2451603830@qq.com>
2023-01-04 18:45:07 +0800

    每日功法

--- test_2023_01_01_clion_TheMinimumNumberOfTheRotatingArray/main.c
+++ test_2023_01_01_clion_TheMinimumNumberOfTheRotatingArray/main.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
 
-    //caushcauhuahdlkajhajsdlasdad;asdasdasdsad;asdasdaduoiaskhcagcbaisucbajkcas;
-    //ddsaaxasdasdasdsaaaasdadsdadd;a
+int minNumberInRotateArray(int* rotateArray, int rotateArrayLen ) {
+
+
+    for(int i = 1; i < rotateArrayLen; i++){
+        if( *rotateArray > *(rotateArray + i)){
+            return *(rotateArray + i);
+        }
+    }
+
+    return *rotateArray;
+}
+
+int main() {
+
+
+    int arr[5] = {3,4,5,1,2};
+
+    int min = minNumberInRotateArray(arr,sizeof(arr)/sizeof(arr[0]));
+    printf("%d\n",min);
     return 0;
 }
