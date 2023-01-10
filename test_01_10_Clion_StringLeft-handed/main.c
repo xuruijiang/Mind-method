6c8d3949f585bab47e1294d6c5ccb530e8f644ff
xuruijiang <2451603830@qq.com>
2023-01-10 12:47:57 +0800

    每日功法

---
+++ test_01_10_Clion_StringLeft-handed/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+
+    char str[] = "abcdefgh";
+    int n = 0;
+
+    scanf("%d",&n);
+
+    char temp;
+
+    char str1[1000] = {0};
+
+    for (int i = n; i < strlen(str); ++i) {
+
+        str1[i - n] = str[i];
+    }
+
+    for (int i = 0; i < n; ++i) {
+
+        str1[strlen(str) - n + i] = str[i];
+    }
+
+
+    printf("%s",str1);
+
+    return 0;
+}
