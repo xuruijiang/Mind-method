38711e3c50f374f9a04d3a5088acb7ee0545446d
xuruijiang <2451603830@qq.com>
2023-01-12 03:26:08 +0800

    每日功法

---
+++ Project/Project/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+
+namespace Day01
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            //string s = Console.ReadLine();
+            //Console.WriteLine(s);
+
+            //Console.Title = "俺的第一个程序";//Title是属性
+
+            string gunName = "AK47";
+            int numberClips = 0;
+            int numberCurrentMagazineBullets = 0;
+            int numberOfBulletsRemaining = 0;
+
+            gunName = Console.ReadLine();
+            numberClips = int.Parse(Console.ReadLine());
+            numberCurrentMagazineBullets = int.Parse(Console.ReadLine());
+            numberOfBulletsRemaining = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("枪名：" + gunName + "   剩余弹夹数：" + numberClips + "   当前弹夹子弹数： " + numberCurrentMagazineBullets + "   剩余子弹数："+ numberOfBulletsRemaining);
+        }
+    }
+}
