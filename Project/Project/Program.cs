4396f56eb60551ebedeb58338940896f722c1ff5
xuruijiang <2451603830@qq.com>
2023-01-13 00:27:20 +0800

    每日功法

--- Project/Project/Program.cs
+++ Project/Project/Program.cs
@@ -18,17 +18,25 @@ namespace Day01
 
             //Console.Title = "俺的第一个程序";//Title是属性
 
+            
             string gunName = "AK47";
             int numberClips = 0;
             int numberCurrentMagazineBullets = 0;
             int numberOfBulletsRemaining = 0;
 
+            Console.WriteLine("请输入枪的名字：");
             gunName = Console.ReadLine();
+            Console.WriteLine("请输入还剩多少个弹夹：");
             numberClips = int.Parse(Console.ReadLine());
+            Console.WriteLine("请输入当前弹夹的子弹数：");
             numberCurrentMagazineBullets = int.Parse(Console.ReadLine());
+            Console.WriteLine("请输入剩余子弹数：");
             numberOfBulletsRemaining = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("枪名：" + gunName + "   剩余弹夹数：" + numberClips + "   当前弹夹子弹数： " + numberCurrentMagazineBullets + "   剩余子弹数："+ numberOfBulletsRemaining);
+            Console.WriteLine("枪名：" + gunName + "   剩余弹夹数：" + numberClips + "   当前弹夹子弹数：" + numberCurrentMagazineBullets + "   剩余子弹数："+ numberOfBulletsRemaining);
+
+            
+
         }
     }
 }
---
+++ Test_23_01_12_VS_Day2/Test_23_01_12_VS_Day2/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+Console.WriteLine("你知道吗");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine(m);
+
+string str = string.Format("我的岁数：{0}", m);
+Console.WriteLine(str);
