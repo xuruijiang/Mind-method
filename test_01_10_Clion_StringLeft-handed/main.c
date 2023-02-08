c9fdef0191db51c1bfce3cb61a6d46d86c6fca29
xuruijiang <2451603830@qq.com>
2023-02-08 18:56:17 +0800

    每日功法

--- test_01_10_Clion_StringLeft-handed/main.c
+++ test_01_10_Clion_StringLeft-handed/main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <string.h>
-
+#include <stdlib.h>
 int main() {
 
 
     char str[] = "abcdefgh";
     int n = 0;
 
-    scanf("%d",&n);
+    scanf(" %d ",&n);
 
     char temp;
 
