f56d68f1221ad0e33326d81e4f3b67f6332588b7
xuruijiang <2451603830@qq.com>
2023-02-09 11:47:29 +0800

    每日功法

--- test_23_01_11_VsCode/main.c
+++ test_23_01_11_VsCode/main.c
@@ -3,6 +3,7 @@
 int main(){
 
     int a = 0;
+    printf("输入：");
     scanf("%d",&a);
     printf("%d\n",a);
 
---
+++ test_23_01_11_VsCode_study/main.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day01{
+    class Program{
+
+        static void Main(string[] args){
+            Console.WriteLine("你好，世界！");
+            Console.ReadLine();
+            Console.WriteLine("nid");
+        }
+    }
+
+}
