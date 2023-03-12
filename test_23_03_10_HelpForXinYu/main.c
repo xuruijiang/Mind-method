b80de2057a1c0318534420c7799702c5a55580b5
xuruijiang <2451603830@qq.com>
2023-03-12 11:39:22 +0800

    每日功法

--- test_23_03_10_HelpForXinYu/main.c
+++ test_23_03_10_HelpForXinYu/main.c
@@ -1,6 +1,40 @@
-#include <stdio.h>
+//
+//  main.c
+//  test_23_03_11_Xcode_HelpFoeXinYu
+//
+//  Created by 徐瑞江 on 2023/3/11.
+//
 
-int main() {
-    printf("Hello, World!\n");
+
+
+#include<stdio.h>
+#include<string.h>
+#include<assert.h>
+//[表情]rncpy模拟实现
+char* my_strncpy(char* dest, char* src,size_t num )
+{
+    assert(dest && src);
+    char* ret = dest;
+    while ( num > 0  && (*dest++ = *src++) )
+    {
+        num--;
+    }
+    if (num != 0)
+    {
+        while (num-- && (dest - ret) < 20)
+        {
+            *dest = 0;
+            dest++;
+        }
+    }
+    return ret;
+}
+int main()
+{
+    char arr1[] = "abcdsadadad";
+    char arr2[20];
+    my_strncpy(arr2, arr1, 20);
+    printf("%s\n", arr2);
+
     return 0;
 }
