286830ddbec54db69b6f4da545261051b3f12347
xuruijiang <2451603830@qq.com>
2023-04-10 15:19:24 +0800

    每日功法

--- test_C++_23_04_09_Clion_LearnC++/main.cpp
+++ test_C++_23_04_09_Clion_LearnC++/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    //shzaddasdsadfdf
-    //dfssdfsdhhdsuhfksjfhsddswosheiohfjsd,fjhvfdsfsd
+
+
+    string s = "ssss";
+    string s1 = "ssss";
+    string s2 = "aaaa";
+
+    int arr[][5] = {{1,3},{2,4}};
     return 0;
 }
