8a4f1dde7dd5d26fa618782e1038269c9ed1ebf9
xuruijiang <2451603830@qq.com>
2023-04-12 20:57:49 +0800

    每日功法

--- test_C++_23_04_12_LearnC++/main.cpp
+++ test_C++_23_04_12_LearnC++/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+using namespace std;
 int a = 0;
+namespace bit
+{
+    int a = 10;
+}
+
+//using namespace bit;
 int main() {
-    int a = 1;
-    printf("%d",::a);
+    //int a = 1;
+    //printf("%d",a);
+    cout << a << endl;
     return 0;
 }
