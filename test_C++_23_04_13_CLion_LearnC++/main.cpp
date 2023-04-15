78cb6844d570c0fd0fa57818a4486c4b264d5a83
xuruijiang <2451603830@qq.com>
2023-04-15 15:02:27 +0800

    每日功法

--- test_C++_23_04_13_CLion_LearnC++/main.cpp
+++ test_C++_23_04_13_CLion_LearnC++/main.cpp
@@ -2,8 +2,16 @@
 
 using std::cout ;
 using std::endl;
+void Func(int a = 0)
+{
+    cout << a << endl;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     cout << "你好 大小姐" << endl;
+
+    Func();//没有传参时，使用参数的默认值
+    Func(10);//传参时，使用指定的实参
     return 0;
 }
