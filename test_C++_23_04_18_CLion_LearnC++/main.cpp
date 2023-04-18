1afd3c59ae070a8b2d6ad4cf12366ccda91b7ddc
xuruijiang <2451603830@qq.com>
2023-04-19 00:05:05 +0800

    每日功法

--- test_C++_23_04_18_CLion_LearnC++/main.cpp
+++ test_C++_23_04_18_CLion_LearnC++/main.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
 using namespace std;
 
+struct ListNode{
+    int val;
+    ListNode* next;
+};
+//--------------------手动分割------------------------//
+
+int Count()
+{
+    static int n = 0;
+    n++;
+
+    return n;
+}//函数结束
+
+//--------------------手动分割------------------------//
+
+int& Count1()
+{
+    int n = 0;
+    n++;
+    return n;
+}//函数结束
+//--------------------手动分割------------------------//
 int main() {
+
+//    int a = 0;
+//    int & b = a;
+//    double c = 0.0;
+//    double &d = c;
+//    printf("%d\n",(int)sizeof(b));
+//    printf("%d\n",(int)sizeof(d));
+//    int &n = Count1();
+//    cout << n << endl;
+//    int m = Count();
+//    Count();
+//    cout << n << endl;
+
+    //int& a = 10;//错
+    int x = Count1();
+    cout << x << endl;
 
     return 0;
 }
