2fe5c506b0bc07a8e89b31f6ffd39007610b869c
xuruijiang <2451603830@qq.com>
2023-04-23 13:32:51 +0800

    每日功法

--- test_C++23_04_22_Clion_BubbleSortingAndQuickSorting/main.cpp
+++ test_C++23_04_22_Clion_BubbleSortingAndQuickSorting/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <unistd.h>
 int main() {
-    printf("你好 你就是垃圾 !");
+    printf("你好 你就是垃圾 !\r");
+    fflush(stdout);
+    sleep(2);
+    printf("nihao\r");
+    fflush(stdout);
     sleep(3);
+    printf("你好 你就是垃圾 !\n");
+
     return 0;
 }
