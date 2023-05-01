21d6b1ef5044972803a7724b1ba8d3afb4d223df
xuruijiang <2451603830@qq.com>
2023-05-01 18:40:13 +0800

    每日功法

---
+++ test_C++_23_4_30_Clion_LearnC++/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using  std::cout;
+using  std::endl;
+class A{
+
+public:
+    void fun(){
+        cout << "hello world" << endl;
+    }
+};
+
+class B{
+public:
+    int a;
+public:
+    B(){
+        cout << "aa" << endl;
+    }
+
+    ~B(){
+
+    }
+};
+
+class C
+{
+public:
+    //这里是给缺省值
+    int _year = 1;
+    int _month = 1;
+    int _day = 1;
+
+public:
+    C(int _year, int _month, int _day)
+    {
+        this->_year = _year;
+        this->_month = _month;
+        this->_day = _day;
+    }
+
+};
+
+
+int main() {
+    //std::cout << "Hello, World!" << std::endl;
+    A a;
+    a.fun();
+    C c(2023,4,30);
+    cout << c._year << endl;
+    return 0;
+}
