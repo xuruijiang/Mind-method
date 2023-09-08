080edfc53a2e71cb15d3519208bc30d54f59d7ba
比昨天强一点 <44053239+xuruijiang@users.noreply.github.com>
2023-09-08 19:28:56 +0800

    每日功法

--- test_23_04_05_Clion_test/main.c
+++ test_23_04_05_Clion_test/main.c
@@ -3,24 +3,32 @@
 
 int main() {
 
+    int a = 0;
+    int b = 0;
+    int sum = 0;
 
-    int a[]= {0};
-    int b = a[0];
-    printf("%d\n",b);
-
-    int c[10] = { };
-    c[1] = 1;
-    printf("%d\n",c[1]);
-
-    int d[10] = {10*1};
+    scanf("%d%d",&a,&b);
+    sum = a + b;
+    printf("%d\n",sum);
 
 
-    int h = a[0];
-    h = 1000;
-
-
-    for (int i = 0; i < 10; ++i) {
-        printf("%d   ",d[i]);
-    }
+//    int a[]= {0};
+//    int b = a[0];
+//    printf("%d\n",b);
+//
+//    int c[10] = { };
+//    c[1] = 1;
+//    printf("%d\n",c[1]);
+//
+//    int d[10] = {10*1};
+//
+//
+//    int h = a[0];
+//    h = 1000;
+//
+//
+//    for (int i = 0; i < 10; ++i) {
+//        printf("%d   ",d[i]);
+//    }
     return 0;
 }
