9f1ab5df7dc419f710614c4f725b670a4a2fcc62
比昨天强一点 <44053239+xuruijiang@users.noreply.github.com>
2023-09-19 17:49:18 +0800

    每日功法

--- test_C_Clion_2023_09_18_Review_C_Language/main.c
+++ test_C_Clion_2023_09_18_Review_C_Language/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 //-----------------------------------------------//
 int greatestCommonDivisor(int num1, int num2)
 {
@@ -13,9 +14,33 @@ int greatestCommonDivisor(int num1, int num2)
     return small;
 }
 //-----------------------------------------------//
+
+//-----------------------------------------------//
 int main() {
 
-    int CommonDivisor = greatestCommonDivisor(9,180);
+//    char ch[20] = "hajdhajhd\0saa";
+//    printf("%s\n",ch);
+    char ch[20] = "hello bit";
+    memset(ch,'a',5);
+    printf("%lu\n",sizeof ch);
+    printf("%s\n",ch);
+
+    for (int i = 0; i < sizeof ch; ++i) {
+        printf("%d, ",ch[i]);
+    }
+    printf("\n");
+    char ch1[20] = {0};
+    strcpy(ch1,ch);
+    for (int i = 0; i < sizeof ch; ++i) {
+        printf("%d, ",ch1[i]);
+    }
+    int num1 = 0;
+    int num2 = 0;
+    scanf("%d%d",&num1,&num2);
+    int CommonDivisor = greatestCommonDivisor(num1,num2);
     printf("%d\n",CommonDivisor);
     return 0;
 }
+//-----------------------------------------------//
+
+//最小公倍数等于两个数想乘再除以最大公约数
---
+++ test_C_Clion_2023_09_19_Review_C_Language/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a = 0;
+    int b = 0;
+    scanf("%d%d",&a,&b);
+
+    a = a^b;
+    b = a^b;
+    a = a^b;
+    printf("a = %d b = %d",a,b);
+
+    return 0;
+}
