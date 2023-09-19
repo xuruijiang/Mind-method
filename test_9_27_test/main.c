3c9ba9db81b4e370cf33a9b10ecca630b12757b9
xuruijiang <2451603830@qq.com>
2023-09-19 21:56:39 +0800

    每日功法

--- test_9_27_test/main.c
+++ test_9_27_test/main.c
@@ -2,6 +2,24 @@
 
 int main(){
 
-printf("Hello World!!!");
-return 0;
+//printf("Hello World!!!");
+
+    #ifndef __STDC_VERSION__
+        puts("这是C90");
+        int ver = 90;
+    #elif __STDC_VERSION__ == 199901L
+        puts("这是C99");
+        int ver = 99;
+    #elif __STDC_VERSION__ == 201112L
+        puts("这是C11");
+        int ver = 11;
+    #else
+        puts("这是什么");
+        int ver = 0;
+    #endif
+
+    int n = 10;
+    //int arr[n] = {0};
+    scanf("%d",&n);
+    return 0;
 }
