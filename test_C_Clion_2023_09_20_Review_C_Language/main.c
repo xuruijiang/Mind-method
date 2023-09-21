a533f9820b3443e22e779e351526eb1f7d110c48
xuruijiang <2451603830@qq.com>
2023-09-21 14:22:07 +0800

    每日功法

--- test_C_Clion_2023_09_20_Review_C_Language/main.c
+++ test_C_Clion_2023_09_20_Review_C_Language/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 //--------------------------------------//
-int binary_search(int *arr, int k, int count)
+int binary_search(int * arr, int k, int count)
 {
     int left = 0;
     int right = count - 1;
@@ -16,15 +16,30 @@ int binary_search(int *arr, int k, int count)
             left = mid + 1;
         }//else if
 
-    }while(arr[mid] != k);
+    }while(arr[mid] != k && left <= right);
     //while(arr[mid] != k)
-    return mid;
+    if(left > right)
+    {
+        return -1;
+    }//if
+    else
+    {
+        return mid;
+    }
+
 }
 //--------------------------------------//
 int main() {
 
     int arr[10] = {1,2,3,4,5,6,7,8,9,10};
-    int index = binary_search(arr,2,10);
-    printf("%d",arr[index]);
+    int index = binary_search(arr,8,sizeof arr/sizeof arr[0]);
+    if(index != -1)
+    {
+        printf("%d\n",arr[index]);
+    }//if
+    else
+    {
+        printf("找不到\n");
+    }//else
     return 0;
 }
