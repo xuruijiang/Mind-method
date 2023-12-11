e73cc59598230570c90e204a2ce9af83fd9e95f6
xuruijiang <2451603830@qq.com>
2023-12-11 18:17:06 +0800

    C语言考研真题

--- test_C_Clion_2023_12_07_TheFifthQuestionOfCLanguagePastExamPaper/main.c
+++ test_C_Clion_2023_12_07_TheFifthQuestionOfCLanguagePastExamPaper/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 //5．编写一个函数，实现对一个整数加密处理，加密方法为：如果整数为
 //负数，则加密之后结果为该数的绝对值；如果整数为非负数，加密方式
@@ -7,7 +9,56 @@
 //及返回值请根据函数功能设计。编写主函数，实现如下功能：输入100
 //个整数，调用函数fun_pass对这些数据进行加密处理，并将加密之后
 //的数据，写入文件data.txt中。
+
+void fun_pass(int * arr,int n)
+{
+    int sum = 0;
+    int sign = 0;
+    for (int i = 0; i < n; ++i) {
+        if(arr[i]  >= 0 )
+        {
+            sign = arr[i] % 10;
+            arr[i] = (arr[i]/10) * 10;
+            sum = sign + 6;
+            if(sign >= 10)
+            {
+                arr[i] = arr[i] + (sum % 10);
+            }
+            else
+            {
+                arr[i] = arr[i] + sum;
+            }
+        }
+        else
+        {
+            arr[i] = -arr[i];
+        }
+        sum = 0;
+
+    }
+
+}
 int main() {
 
+    FILE *pf = fopen("/Users/xuruijiang/Downloads/data.txt","a");
+    srand((unsigned ) time(NULL));
+    int arr[100] = {0};
+
+    for (int i = 0; i < 100; ++i) {
+        arr[i] = rand()%1000;
+    }
+    for (int i = 0; i < 100; ++i) {
+        printf("%4d ",arr[i]);
+    }
+
+    fun_pass(arr,sizeof(arr)/sizeof(arr[0]));
+    printf("\n");
+    for (int i = 0; i < 100; ++i) {
+        fprintf(pf,"%d ",arr[i]);
+        printf("%4d ",arr[i]);
+    }
+
+    fclose(pf);
+    pf = NULL;
     return 0;
 }
